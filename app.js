8222968a4eb5e2cdb7ee5d19501b68b025280e97
NICCK08 <65999730+NICCK08@users.noreply.github.com>
2022-10-02 20:33:45 +0530

    Update js file

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
-const btn = document.querySelector(".btn");
-const textArea = document.querySelector(".textarea");
-const translation = document.querySelector(".translated");
-const url = "https://api.funtranslations.com/translate/groot.json";
+var btnTranslate =document.querySelector("#btn-translate");
+var txtInput = document.querySelector(".txt-input");
+var outputDiv = document.querySelector(".output-div");
 
-btn.addEventListener("click", () => {
-  let value = textArea.value;
-  const newUrl = url + "?text=" + value;
 
-  fetch(newUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      translation.innerHTML = data.contents.translated;
-    })
-    .catch((error) => alert("Something went wrong", error)); 
-});
+var serverURL ="https://api.funtranslations.com/translate/groot.json"
+
+function getTranslationURL(text){
+    return serverURL + "?" +"text=" +text
+}
+
+function errorHandler(error) {
+    console.log("error occured", error)
+    alert("something wrong with server");
+}
+
+function clickHandler(){
+
+    var inputText = txtInput.value; //taking input
+    console.log(inputText, "inputText");
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated
+            outputDiv.innerText = translatedText; //output
+        })
+        .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickHandler);
